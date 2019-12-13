3070548d12c2079e0eb4ce8caa19907e2b9087ca
Thomy <thomyfarhan@gmail.com>
2019-12-13 22:21:19 +0700

    [Database Read] Make Model as Entity

--- app/src/main/java/com/aesthomic/readinglog/database/Read.kt
+++ app/src/main/java/com/aesthomic/readinglog/database/Read.kt
@@ -1,9 +1,23 @@
 package com.aesthomic.readinglog.database
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "read")
 data class Read(
+    @PrimaryKey(autoGenerate = true)
     var id: Long = 0L,
+
+    @ColumnInfo(name = "start_time_millis")
     var startTimeMillis: Long = 0L,
+
+    @ColumnInfo(name = "end_time_millis")
     var endTimeMillis: Long = 0L,
+
+    @ColumnInfo(name = "book_name")
     var bookName: String = "",
+
+    @ColumnInfo(name = "chapter")
     var chapter: Int = 0
 )
