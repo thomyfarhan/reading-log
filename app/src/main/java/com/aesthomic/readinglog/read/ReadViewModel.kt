ecdc36ff8bf9d04995fe84df4004f6a2bbb93ce9
Thomy <thomyfarhan@gmail.com>
2019-12-14 15:24:13 +0700

    [Fragment Read] Add Coroutines flow on ViewModel

--- app/src/main/java/com/aesthomic/readinglog/read/ReadViewModel.kt
+++ app/src/main/java/com/aesthomic/readinglog/read/ReadViewModel.kt
@@ -3,7 +3,21 @@ package com.aesthomic.readinglog.read
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import com.aesthomic.readinglog.database.ReadDao
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
 
 class ReadViewModel(
     private val database: ReadDao,
-    application: Application): AndroidViewModel(application) {}
+    application: Application): AndroidViewModel(application) {
+
+    private val viewModelJob = Job()
+
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    override fun onCleared() {
+        super.onCleared()
+        uiScope.cancel()
+    }
+}
