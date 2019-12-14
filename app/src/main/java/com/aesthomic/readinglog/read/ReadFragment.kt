fbd53b321f739858ce08bdf3c9e790b4bceca0e8
Thomy <thomyfarhan@gmail.com>
2019-12-14 14:49:06 +0700

    [Fragment Read] Init View Model

--- app/src/main/java/com/aesthomic/readinglog/read/ReadFragment.kt
+++ app/src/main/java/com/aesthomic/readinglog/read/ReadFragment.kt
@@ -1,20 +1,29 @@
 package com.aesthomic.readinglog.read
 
 
+import android.app.Application
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.aesthomic.readinglog.R
+import com.aesthomic.readinglog.database.ReadDao
+import com.aesthomic.readinglog.database.ReadingLogDatabase
 import com.aesthomic.readinglog.databinding.FragmentReadBinding
 
 class ReadFragment : Fragment() {
 
     private lateinit var binding: FragmentReadBinding
+    private lateinit var application: Application
+    private lateinit var database: ReadDao
+    private lateinit var viewModelFactory: ReadViewModelFactory
+    private lateinit var viewModel: ReadViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,10 +32,21 @@ class ReadFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater,
             R.layout.fragment_read, container, false)
 
+        initViewModel()
+
         binding.rvRead.layoutManager = LinearLayoutManager(requireContext())
         binding.rvRead.adapter = ReadAdapter()
 
         return binding.root
+    }
+
+    private fun initViewModel() {
+        application = requireNotNull(this.activity).application
+        database = ReadingLogDatabase.getInstance(application).readDao
+
+        viewModelFactory = ReadViewModelFactory(database, application)
+        viewModel = ViewModelProviders.of(
+            this, viewModelFactory).get(ReadViewModel::class.java)
     }
 
 
