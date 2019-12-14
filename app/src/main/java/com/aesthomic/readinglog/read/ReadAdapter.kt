374914b0310ecf1a6456f9e50eb275395a8164d7
Thomy <thomyfarhan@gmail.com>
2019-12-14 13:45:43 +0700

    [Fragment Read] Add bind function on ViewHolder

--- app/src/main/java/com/aesthomic/readinglog/read/ReadAdapter.kt
+++ app/src/main/java/com/aesthomic/readinglog/read/ReadAdapter.kt
@@ -3,12 +3,15 @@ package com.aesthomic.readinglog.read
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.aesthomic.readinglog.database.Read
+import kotlinx.android.synthetic.main.item_list_read.view.*
 
 /**
  * Adapter creates a View Holder and fills it with
  * data for the Recycler View to display
  */
-class ReadAdapter: RecyclerView.Adapter<ReadAdapter.ReadViewHolder>() {
+class ReadAdapter(val listReads: MutableList<Read>):
+    RecyclerView.Adapter<ReadAdapter.ReadViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReadViewHolder {
 
@@ -25,5 +28,10 @@ class ReadAdapter: RecyclerView.Adapter<ReadAdapter.ReadViewHolder>() {
     /**
      * Layout for the items to be displayed inside the RecyclerView
      */
-    inner class ReadViewHolder(view: View): RecyclerView.ViewHolder(view) {}
+    inner class ReadViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
+
+        fun bind(read: Read) {
+            view.tvStartTime.text = read.startTimeMillis.toString()
+        }
+    }
 }
