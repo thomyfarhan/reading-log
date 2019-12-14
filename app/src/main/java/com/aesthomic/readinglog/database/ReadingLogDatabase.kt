002f7a4229b6a9f69a064f716211eaff04da6e96
Thomy <thomyfarhan@gmail.com>
2019-12-14 09:56:46 +0700

    [Database] Add instance for building database

--- app/src/main/java/com/aesthomic/readinglog/database/ReadingLogDatabase.kt
+++ app/src/main/java/com/aesthomic/readinglog/database/ReadingLogDatabase.kt
@@ -1,9 +1,37 @@
 package com.aesthomic.readinglog.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Read::class], version = 1)
 abstract class ReadingLogDatabase: RoomDatabase() {
 
+    abstract val readDao: ReadDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: ReadingLogDatabase? = null
+
+        fun getInstance(context: Context): ReadingLogDatabase {
+
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context,
+                        ReadingLogDatabase::class.java,
+                        "reading_log_database")
+                        .build()
+
+                    INSTANCE = instance
+                }
+
+                return instance
+            }
+        }
+    }
 }
