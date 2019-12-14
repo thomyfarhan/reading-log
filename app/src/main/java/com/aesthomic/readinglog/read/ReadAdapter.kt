bbd788fee7d404b630621fb0910c1aa536e22da1
Thomy <thomyfarhan@gmail.com>
2019-12-14 13:31:14 +0700

    [Fragment Read] Add ViewHolder

--- app/src/main/java/com/aesthomic/readinglog/read/ReadAdapter.kt
+++ app/src/main/java/com/aesthomic/readinglog/read/ReadAdapter.kt
@@ -1,9 +1,16 @@
 package com.aesthomic.readinglog.read
 
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
 /**
  * Adapter creates a View Holder and fills it with
  * data for the Recycler View to display
  */
-class ReadAdapter: RecyclerView.Adapter<>() {}
+class ReadAdapter: RecyclerView.Adapter<ReadAdapter.ReadViewHolder>() {
+
+    /**
+     * Layout for the items to be displayed inside the RecyclerView
+     */
+    inner class ReadViewHolder(view: View): RecyclerView.ViewHolder(view) {}
+}
