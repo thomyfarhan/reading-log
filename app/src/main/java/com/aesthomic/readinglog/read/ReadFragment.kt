1462c8b7513b6f99097617757f4df2badfe0867c
Thomy <thomyfarhan@gmail.com>
2019-12-14 14:02:09 +0700

    [Fragment Read] Set up RecyclerView Adapter

--- app/src/main/java/com/aesthomic/readinglog/read/ReadFragment.kt
+++ app/src/main/java/com/aesthomic/readinglog/read/ReadFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.aesthomic.readinglog.R
 import com.aesthomic.readinglog.databinding.FragmentReadBinding
@@ -21,6 +22,9 @@ class ReadFragment : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate(inflater,
             R.layout.fragment_read, container, false)
+
+        binding.rvRead.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvRead.adapter = ReadAdapter()
 
         return binding.root
     }
