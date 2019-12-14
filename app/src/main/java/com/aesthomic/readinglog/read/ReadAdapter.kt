129a038714d6ccf30b7d3b4d2aac6f5b23b524af
Thomy <thomyfarhan@gmail.com>
2019-12-14 13:29:37 +0700

    [Fragment Read] Add Adapter

---
+++ app/src/main/java/com/aesthomic/readinglog/read/ReadAdapter.kt
@@ -0,0 +1,9 @@
+package com.aesthomic.readinglog.read
+
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Adapter creates a View Holder and fills it with
+ * data for the Recycler View to display
+ */
+class ReadAdapter: RecyclerView.Adapter<>() {}
