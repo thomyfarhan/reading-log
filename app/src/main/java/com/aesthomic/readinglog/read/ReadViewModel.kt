0bc69a43275b457020da6a5b61302b613890e467
Thomy <thomyfarhan@gmail.com>
2019-12-15 10:54:31 +0700

    [Fragment Read] Implement action to update and delete

--- app/src/main/java/com/aesthomic/readinglog/read/ReadViewModel.kt
+++ app/src/main/java/com/aesthomic/readinglog/read/ReadViewModel.kt
@@ -2,6 +2,8 @@ package com.aesthomic.readinglog.read
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.aesthomic.readinglog.database.Read
 import com.aesthomic.readinglog.database.ReadDao
 import kotlinx.coroutines.*
@@ -14,7 +16,41 @@ class ReadViewModel(
 
     private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
+    private val currentRead = MutableLiveData<Read?>()
+
     val reads = database.getAllReads()
+
+    init {
+        initializeCurrentRead()
+    }
+
+    private fun initializeCurrentRead() {
+        uiScope.launch {
+            currentRead.value = getCurrentFromDatabase()
+        }
+    }
+
+    private suspend fun getCurrentFromDatabase(): Read? {
+        return withContext(Dispatchers.IO) {
+            var read = database.getCurrentRead()
+            if (read?.startTimeMillis != read?.endTimeMillis) {
+                read = null
+            }
+            read
+        }
+    }
+
+    private suspend fun clear() {
+        withContext(Dispatchers.IO) {
+            database.clear()
+        }
+    }
+
+    private suspend fun update(read: Read) {
+        withContext(Dispatchers.IO) {
+            database.update(read)
+        }
+    }
 
     private suspend fun insert(read: Read) {
         withContext(Dispatchers.IO) {
@@ -22,10 +58,26 @@ class ReadViewModel(
         }
     }
 
+    fun onDelete() {
+        uiScope.launch {
+            clear()
+            currentRead.value = null
+        }
+    }
+
+    fun onStopReading() {
+        uiScope.launch {
+            val oldRead = currentRead.value ?: return@launch
+            oldRead.endTimeMillis = System.currentTimeMillis()
+            update(oldRead)
+        }
+    }
+
     fun onStartReading() {
         uiScope.launch {
             val read = Read(startTimeMillis = System.currentTimeMillis())
             insert(read)
+            currentRead.value = getCurrentFromDatabase()
         }
     }
 
