75c62f525fd9de5cfef017a1f800a0d2175b83b3
Thomy <thomyfarhan@gmail.com>
2019-12-15 17:22:54 +0700

    [Fragment Read Book] Add Fragment

---
+++ app/src/main/java/com/aesthomic/readinglog/readbook/ReadBookFragment.kt
@@ -0,0 +1,22 @@
+package com.aesthomic.readinglog.readbook
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.aesthomic.readinglog.R
+
+class ReadBookFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_read_book, container, false)
+    }
+
+
+}
