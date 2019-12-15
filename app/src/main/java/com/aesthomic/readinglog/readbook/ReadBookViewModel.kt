1a21ac2c5e00a27cb83bd531930784334e9da6da
Thomy <thomyfarhan@gmail.com>
2019-12-15 19:26:07 +0700

    [Fragment Read Book] Add functions in ViewModel to update data

--- app/src/main/java/com/aesthomic/readinglog/readbook/ReadBookViewModel.kt
+++ app/src/main/java/com/aesthomic/readinglog/readbook/ReadBookViewModel.kt
@@ -1,10 +1,55 @@
 package com.aesthomic.readinglog.readbook
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.aesthomic.readinglog.database.Read
 import com.aesthomic.readinglog.database.ReadDao
+import kotlinx.coroutines.*
 
 class ReadBookViewModel (
     private val readKey: Long,
-    private val database: ReadDao,
-    application: Application): AndroidViewModel(application) {}
+    private val database: ReadDao): ViewModel() {
+
+    private val viewModelJob = Job()
+
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    private val _eventSubmit = MutableLiveData<Boolean>()
+    val eventSubmit: LiveData<Boolean>
+        get() = _eventSubmit
+
+    fun updateRead(bookName: String, page: Int) {
+        uiScope.launch {
+            val read = getReadById(readKey) ?: return@launch
+            read.bookName = bookName
+            read.chapter = page
+            update(read)
+        }
+    }
+
+    private suspend fun getReadById(key: Long): Read? {
+        return withContext(Dispatchers.IO) {
+            database.get(key)
+        }
+    }
+
+    private suspend fun update(read: Read) {
+        withContext(Dispatchers.IO) {
+            database.update(read)
+        }
+    }
+
+    fun onDataSubmitted() {
+        _eventSubmit.value = true
+    }
+
+    fun onSubmitDone() {
+        _eventSubmit.value = false
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+}
