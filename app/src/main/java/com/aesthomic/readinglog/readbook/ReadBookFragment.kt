c693b8ec6b0c8bcd3b89d9074c531d12bbe13b94
Thomy <thomyfarhan@gmail.com>
2019-12-15 18:03:11 +0700

    [Fragment Read Book] Init View Model

--- app/src/main/java/com/aesthomic/readinglog/readbook/ReadBookFragment.kt
+++ app/src/main/java/com/aesthomic/readinglog/readbook/ReadBookFragment.kt
@@ -7,13 +7,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 
 import com.aesthomic.readinglog.R
+import com.aesthomic.readinglog.database.ReadingLogDatabase
 import com.aesthomic.readinglog.databinding.FragmentReadBookBinding
 
 class ReadBookFragment : Fragment() {
 
     private lateinit var binding: FragmentReadBookBinding
+    private lateinit var viewModel: ReadBookViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,7 +25,20 @@ class ReadBookFragment : Fragment() {
         binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_read_book, container, false)
 
+        initViewModel()
+
         return binding.root
+    }
+
+    private fun initViewModel() {
+        val application = requireNotNull(this.activity).application
+        val database = ReadingLogDatabase.getInstance(application).readDao
+        val readKey = ReadBookFragmentArgs.fromBundle(requireArguments()).readKey
+
+        val viewModelFactory = ReadBookViewModelFactory(
+            readKey, database, application)
+        viewModel = ViewModelProviders.of(this, viewModelFactory)
+            .get(ReadBookViewModel::class.java)
     }
 
 
