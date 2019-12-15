e2040667aa067d0bd310a4cd1215e8137ab286b1
Thomy <thomyfarhan@gmail.com>
2019-12-15 19:25:16 +0700

    [Fragment Read Book] Remove application parameter on ViewModelFactory

--- app/src/main/java/com/aesthomic/readinglog/readbook/ReadBookViewModelFactory.kt
+++ app/src/main/java/com/aesthomic/readinglog/readbook/ReadBookViewModelFactory.kt
@@ -1,6 +1,5 @@
 package com.aesthomic.readinglog.readbook
 
-import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.aesthomic.readinglog.database.ReadDao
@@ -8,13 +7,12 @@ import java.lang.IllegalArgumentException
 
 class ReadBookViewModelFactory(
     private val readKey: Long,
-    private val database: ReadDao,
-    private val application: Application): ViewModelProvider.Factory {
+    private val database: ReadDao): ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if(modelClass.isAssignableFrom(ReadBookViewModel::class.java)) {
-            return ReadBookViewModel(readKey, database, application) as T
+            return ReadBookViewModel(readKey, database) as T
         }
         throw IllegalArgumentException("Unknown ViewModel Class")
     }
