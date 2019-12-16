a66bdb88f5e689e63c39d019cd1b9b12776eab9e
Thomy <thomyfarhan@gmail.com>
2019-12-17 00:37:24 +0700

    [Fragment Read Detail] Init ViewModel

--- app/src/main/java/com/aesthomic/readinglog/readdetail/ReadDetailFragment.kt
+++ app/src/main/java/com/aesthomic/readinglog/readdetail/ReadDetailFragment.kt
@@ -7,13 +7,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 
 import com.aesthomic.readinglog.R
+import com.aesthomic.readinglog.database.ReadingLogDatabase
 import com.aesthomic.readinglog.databinding.FragmentReadDetailBinding
 
 class ReadDetailFragment : Fragment() {
 
     private lateinit var binding: FragmentReadDetailBinding
+    private lateinit var viewModel: ReadDetailViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,7 +25,19 @@ class ReadDetailFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater,
             R.layout.fragment_read_detail, container, false)
 
+        initViewModel()
+
         return binding.root
+    }
+
+    private fun initViewModel() {
+        val application = requireNotNull(this.activity).application
+        val database = ReadingLogDatabase.getInstance(application).readDao
+        val args = ReadDetailFragmentArgs.fromBundle(requireArguments())
+
+        val viewModelFactory = ReadDetailViewModelFactory(args.readKey, database)
+        viewModel = ViewModelProviders.of(this, viewModelFactory)
+            .get(ReadDetailViewModel::class.java)
     }
 
 
