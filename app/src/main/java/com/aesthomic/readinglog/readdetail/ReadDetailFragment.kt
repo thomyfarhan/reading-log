0ee5686708638b8e1c97ff9d501276bee2a407ff
Thomy <thomyfarhan@gmail.com>
2019-12-16 09:45:29 +0700

    [Fragment Read Detail] Change layout and implement binding

--- app/src/main/java/com/aesthomic/readinglog/readdetail/ReadDetailFragment.kt
+++ app/src/main/java/com/aesthomic/readinglog/readdetail/ReadDetailFragment.kt
@@ -6,16 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 
 import com.aesthomic.readinglog.R
+import com.aesthomic.readinglog.databinding.FragmentReadDetailBinding
 
 class ReadDetailFragment : Fragment() {
+
+    private lateinit var binding: FragmentReadDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_read_detail, container, false)
+        binding = DataBindingUtil.inflate(inflater,
+            R.layout.fragment_read_detail, container, false)
+
+        return binding.root
     }
 
 
