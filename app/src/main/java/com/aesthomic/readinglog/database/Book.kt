118692971803421d79d7d7e582a5fa1cbd9a1dd5
Thomy <thomyfarhan@gmail.com>
2019-12-19 00:07:09 +0700

    [Database Book] Make Model as Entity

--- app/src/main/java/com/aesthomic/readinglog/database/Book.kt
+++ app/src/main/java/com/aesthomic/readinglog/database/Book.kt
@@ -1,13 +1,23 @@
 package com.aesthomic.readinglog.database
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "books")
 data class Book(
+    @PrimaryKey(autoGenerate = true)
     var id: Long = 0L,
 
+    @ColumnInfo(name = "title")
     var title: String = "",
 
+    @ColumnInfo(name = "page")
     var page: Int = 0,
 
+    @ColumnInfo(name = "desc")
     var desc: String = "",
 
+    @ColumnInfo(name = "photo")
     var photo: String = ""
 )
