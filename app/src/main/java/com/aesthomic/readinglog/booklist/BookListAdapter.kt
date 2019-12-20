24089961c7bf329552bf20cc2acc2e2dbc1fe53b
Thomy <thomyfarhan@gmail.com>
2019-12-20 21:02:48 +0700

    [Fragment Book List] Add Adapter

---
+++ app/src/main/java/com/aesthomic/readinglog/booklist/BookListAdapter.kt
@@ -0,0 +1,10 @@
+package com.aesthomic.readinglog.booklist
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class BookListAdapter {
+
+    class BookListViewHolder(view: View):
+        RecyclerView.ViewHolder(view) {}
+}
