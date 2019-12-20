7a5bdea1125d74cfc95c94b6d7d1b52a6f6b6ed1
Thomy <thomyfarhan@gmail.com>
2019-12-20 10:04:44 +0700

    [Fragment Book List] Add ViewModel

---
+++ app/src/main/java/com/aesthomic/readinglog/booklist/BookListViewModel.kt
@@ -0,0 +1,7 @@
+package com.aesthomic.readinglog.booklist
+
+import androidx.lifecycle.ViewModel
+import com.aesthomic.readinglog.database.BookDao
+
+class BookListViewModel(
+    private val dbBook: BookDao): ViewModel() {}
