2a97b9923971e36913ee38ad125e816442ab2df2
Thomy <thomyfarhan@gmail.com>
2019-12-23 21:34:49 +0700

    [Framgent Book Picture Dialog] Change layout & implement binding

--- app/src/main/java/com/aesthomic/readinglog/readbook/BookPictureDialogFragment.kt
+++ app/src/main/java/com/aesthomic/readinglog/readbook/BookPictureDialogFragment.kt
@@ -6,16 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 
 import com.aesthomic.readinglog.R
+import com.aesthomic.readinglog.databinding.FragmentBookPictureDialogBinding
 
 class BookPictureDialogFragment : Fragment() {
+
+    private lateinit var binding: FragmentBookPictureDialogBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_book_picture_dialog, container, false)
+        binding = DataBindingUtil.inflate(inflater,
+            R.layout.fragment_book_picture_dialog, container, false)
+
+        return binding.root
     }
 
 
