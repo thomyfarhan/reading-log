21638bb00a4659e8fed6b0f942545ad409604e95
Thomy <thomyfarhan@gmail.com>
2019-12-26 13:40:26 +0700

    [Application] Init Modules

--- app/src/main/java/com/aesthomic/readinglog/app/App.kt
+++ app/src/main/java/com/aesthomic/readinglog/app/App.kt
@@ -1,11 +1,24 @@
 package com.aesthomic.readinglog.app
 
 import android.app.Application
+import com.aesthomic.readinglog.app.di.databaseModule
+import com.aesthomic.readinglog.app.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class App: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        startKoin {
+            androidContext(this@App)
+
+            val listModules = listOf(
+                viewModelModule,
+                databaseModule
+            )
+            modules(listModules)
+        }
     }
 
 
