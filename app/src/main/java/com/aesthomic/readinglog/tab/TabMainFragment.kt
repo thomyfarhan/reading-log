f7875f17a8d1f3cdc2f4c7f3af0a48811d3f8bde
Thomy <thomyfarhan@gmail.com>
2020-01-02 22:13:17 +0700

    [Fragment Tab Main] Implement Adapter to view in layout

--- app/src/main/java/com/aesthomic/readinglog/tab/TabMainFragment.kt
+++ app/src/main/java/com/aesthomic/readinglog/tab/TabMainFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 
 import com.aesthomic.readinglog.R
@@ -22,7 +23,17 @@ class TabMainFragment : Fragment() {
         binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_tab_main, container, false)
 
+        setupTab()
+
         return binding.root
+    }
+
+    private fun setupTab() {
+        val tabMainAdapter = TabMainAdapter(requireContext(), childFragmentManager)
+        binding.vpMain.adapter = tabMainAdapter
+        binding.tlMain.setupWithViewPager(binding.vpMain)
+
+        (activity as AppCompatActivity).supportActionBar?.elevation = 0f
     }
 
 
