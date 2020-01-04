dc7672691c8391607032fcaa0a89e79c9a524aa4
Thomy <thomyfarhan@gmail.com>
2020-01-04 17:31:01 +0700

    [Fragment Book Detail Dialog] Initialize view model by sharedviewmodel

--- app/src/main/java/com/aesthomic/readinglog/book/BookDetailDialogFragment.kt
+++ app/src/main/java/com/aesthomic/readinglog/book/BookDetailDialogFragment.kt
@@ -10,10 +10,12 @@ import androidx.fragment.app.DialogFragment
 
 import com.aesthomic.readinglog.R
 import com.aesthomic.readinglog.databinding.FragmentBookDetailDialogBinding
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class BookDetailDialogFragment : DialogFragment() {
 
     private lateinit var binding: FragmentBookDetailDialogBinding
+    private val viewModel: BookViewModel by sharedViewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +23,12 @@ class BookDetailDialogFragment : DialogFragment() {
     ): View? {
         binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_book_detail_dialog, container, false)
+
+        binding.svBookDetailDesc.setMaxHeight(500)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        viewModel.setBookKey(BookDetailDialogFragmentArgs.fromBundle(arguments!!).bookKey)
 
         return binding.root
     }
