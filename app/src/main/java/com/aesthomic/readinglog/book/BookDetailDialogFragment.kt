8f50d2a7e7faca9ad286b0e1386d1bec8876a755
Thomy <thomyfarhan@gmail.com>
2020-01-04 12:43:25 +0700

    [Fragment Book Detail Dialog] Change layout and implement binding

--- app/src/main/java/com/aesthomic/readinglog/book/BookDetailDialogFragment.kt
+++ app/src/main/java/com/aesthomic/readinglog/book/BookDetailDialogFragment.kt
@@ -2,21 +2,27 @@ package com.aesthomic.readinglog.book
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
 
 import com.aesthomic.readinglog.R
+import com.aesthomic.readinglog.databinding.FragmentBookDetailDialogBinding
 
 class BookDetailDialogFragment : DialogFragment() {
+
+    private lateinit var binding: FragmentBookDetailDialogBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_book_detail_dialog, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_book_detail_dialog, container, false)
+
+        return binding.root
     }
 
 
