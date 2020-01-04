e0eb116834542940b8d381c87c9a282997571747
Thomy <thomyfarhan@gmail.com>
2020-01-04 20:38:37 +0700

    [Fragment Edit Book] Init ViewModel and Set Book Field

--- app/src/main/java/com/aesthomic/readinglog/editbook/EditBookFragment.kt
+++ app/src/main/java/com/aesthomic/readinglog/editbook/EditBookFragment.kt
@@ -7,13 +7,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 
 import com.aesthomic.readinglog.R
+import com.aesthomic.readinglog.book.BookViewModel
 import com.aesthomic.readinglog.databinding.FragmentEditBookBinding
+import com.aesthomic.readinglog.util.setSrcByUriString
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class EditBookFragment : Fragment() {
 
     private lateinit var binding: FragmentEditBookBinding
+    private val viewModel: BookViewModel by sharedViewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +26,21 @@ class EditBookFragment : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_edit_book, container, false)
+
+        binding.lifecycleOwner = this
+
+        viewModel.setBookKey(EditBookFragmentArgs.fromBundle(arguments!!).readKey)
+
+        viewModel.book.observe(this, Observer {
+            it?.let {
+                with(binding) {
+                    ivEditBookPhoto.setSrcByUriString(it.photo)
+                    etEditBookTitle.setText(it.title)
+                    etEditBookPage.setText(it.page.toString())
+                    etEditBookDesc.setText(it.desc)
+                }
+            }
+        })
 
         return binding.root
     }
