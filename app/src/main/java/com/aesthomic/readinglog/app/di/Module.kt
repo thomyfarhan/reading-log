a7e849872e059114a9960b3bdb3a1c12b1fa9826
Thomy <thomyfarhan@gmail.com>
2020-01-04 09:47:10 +0700

    [Fragment Book] Add Module for Book View Model

--- app/src/main/java/com/aesthomic/readinglog/app/di/Module.kt
+++ app/src/main/java/com/aesthomic/readinglog/app/di/Module.kt
@@ -1,11 +1,13 @@
 package com.aesthomic.readinglog.app.di
 
 import android.app.Application
+import com.aesthomic.readinglog.book.BookViewModel
 import com.aesthomic.readinglog.database.BookDao
 import com.aesthomic.readinglog.database.ReadDao
 import com.aesthomic.readinglog.database.ReadingLogDatabase
 import com.aesthomic.readinglog.readbook.ReadBookViewModel
 import org.koin.android.ext.koin.androidApplication
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
@@ -13,6 +15,8 @@ val viewModelModule = module {
     scope(named(Scopes.READ_BOOK)) {
         scoped { ReadBookViewModel(-1L, get(), get()) }
     }
+
+    viewModel { BookViewModel(get()) }
 }
 
 val databaseModule = module {
