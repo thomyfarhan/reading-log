903d9fcd954351b1691265577774a6a517742578
Thomy <thomyfarhan@gmail.com>
2020-01-04 12:41:42 +0700

    [Fragment Book Detail Dialog] Add Dialog Fragment

---
+++ app/src/main/java/com/aesthomic/readinglog/book/BookDetailDialogFragment.kt
@@ -0,0 +1,23 @@
+package com.aesthomic.readinglog.book
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+
+import com.aesthomic.readinglog.R
+
+class BookDetailDialogFragment : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_book_detail_dialog, container, false)
+    }
+
+
+}
