1efde8f8fda0cbe57b474321a6b8832a786a6d4b
Thomy <thomyfarhan@gmail.com>
2020-01-04 19:13:30 +0700

    [Fragment Edit Book] Change layout and implement binding

--- app/src/main/java/com/aesthomic/readinglog/editbook/EditBookFragment.kt
+++ app/src/main/java/com/aesthomic/readinglog/editbook/EditBookFragment.kt
@@ -6,16 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 
 import com.aesthomic.readinglog.R
+import com.aesthomic.readinglog.databinding.FragmentEditBookBinding
 
 class EditBookFragment : Fragment() {
+
+    private lateinit var binding: FragmentEditBookBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_edit_book, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_edit_book, container, false)
+
+        return binding.root
     }
 
 
